84f1bf283cd3a335c7ea1ce166ca996f574097e5
Amanda Ross <amikaross@gmail.com>
2023-01-05 07:49:09 -0700

    Change status code for post to :created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save 
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: :created
     else 
       render json: ErrorSerializer.missing_attributes(user.errors.full_messages), status: :bad_request
     end
