4726225a370a3a9a8e1afc99ee6d562164a22763
Amanda Ross <amikaross@gmail.com>
2023-01-06 10:10:27 -0700

    Breweries Service - add brewery poro and filter through objects

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::BreweriesController < ApplicationController
   def search
     user = User.find(params[:user_id])
-    city = user.city
-    state = user.state
-    render json: BreweriesService.search_by_city_state(city, state), status: 200
+    city, state = [user.city, user.state]
+    breweries = BreweriesService.search_by_city_state(city, state)
+    render json: BrewerySerializer.new(breweries), status: 200
   end
 end
