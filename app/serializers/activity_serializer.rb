95b2a96788166762901906f1b04b6f01613652c1
Amanda Ross <amikaross@gmail.com>
2023-01-06 07:02:01 -0700

    Remove brewery_id from serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,5 @@
 class ActivitySerializer
   include JSONAPI::Serializer
   belongs_to :user
-  attributes :brewery_id, :distance, :calories, :num_drinks, :drink_type, :brewery_name
+  attributes :distance, :calories, :num_drinks, :drink_type, :brewery_name
 end
