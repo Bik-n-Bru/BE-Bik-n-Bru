d38724eb43fdd1117fe835eb9017940380626341
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-01-06 10:35:09 -0700

    Refactor in breweries service.rb file

--- app/services/breweries_service.rb
+++ app/services/breweries_service.rb
@@ -8,11 +8,15 @@ class BreweriesService
 
   def self.get_url(url, city, state)
     response = conn(city, state).get(url)
-    JSON.parse(response.body, symbolize_names: true)
+    parsed_response(response)
   end
 
   def self.conn(city, state)
     Faraday.new(url: "https://api.openbrewerydb.org/", 
       params: { "by_city": city, "by_state": state })
+  end
+
+  def self.parsed_response(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
